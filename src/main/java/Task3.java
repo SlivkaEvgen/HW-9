89451d946999e925278120885a91d84458af78ee
zeka <allrent3@gmail.com>
2021-05-10 19:43:10 +0300

    ddd

--- src/main/java/Task3.java
+++ src/main/java/Task3.java
@@ -1,25 +1,22 @@
  //Gotovo!!!
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
-class Words2 {
+ class Words2 {
     public Words2() {
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         ArrayList<String> words = new ArrayList<>();
-        words.add(0, "the");
-        words.add(1, "day");
-        words.add(2, "is");
-        words.add(3, "sunny");
-        words.add(4, "the");
-        words.add(5, "the");
-        words.add(6, "the");
-        words.add(7, "sunny");
-        words.add(8, "is");
-        words.add(9, "is");
-
+        FileReader reader = new FileReader("/Users/zeka/IdeaProjects/HW-9/src/main/resources/words.txt");
+        Scanner sc = new Scanner(reader);
+        while (sc.hasNext()) {
+            words.add(sc.next());
+        }
         Map<String, Integer> map = new HashMap<>();
         for (String word : words) {
             map.put(word, map.getOrDefault(word, 0) + 1);
